78b338004aa5ca84a5febbfd603dbfeb7b1cb73f
LinLight <a.bayramova2007@gmail.com>
2026-08-21 10:43:48 +0400

    Fix menu navigation

--- src/components/WineBanner.jsx
+++ src/components/WineBanner.jsx
@@ -1,7 +1,10 @@
 import './WineBanner.css'
+import { useNavigate } from "react-router-dom";
 
 function WineBanner() {
+  const navigate = useNavigate();
   return (
+    
     <section className="wine-banner">
       <img
         src={`${import.meta.env.BASE_URL}img/winebanner.png`}
@@ -11,7 +14,12 @@ function WineBanner() {
       <div className="wine-banner-overlay">
         <div className="wine-banner-content">
           <h2 className="wine-banner-title">Finest Wine Selection</h2>
-          <button className="wine-banner-button">Menu</button>
+          <button className="wine-banner-button" onClick={() => {
+    navigate("/menu");
+    window.scrollTo(0, 0);
+  }}>Menu
+
+          </button>
         </div>
       </div>
     </section>
